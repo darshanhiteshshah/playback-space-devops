23f1f5e7cbed1d4e8ac287434a46af03a2200b36
Darshan Shah <darshanhiteshshah@gmail.com>
2026-05-02 19:23:31 +0530

    fix: cors issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,12 +6,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
-    origin: "*",
+    origin: "http://13.53.129.124:5173",
     credentials: true,
   })
 );
-
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
