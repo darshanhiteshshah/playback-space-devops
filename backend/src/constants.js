0a3a62bb0c3a88a8e3725a572cb16254414450b4
Darshan Shah <darshanhiteshshah@gmail.com>
2026-05-03 09:30:12 +0530

    fix: cookie path issue

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -1,9 +1,8 @@
 export const DB_NAME = "PlaybackSpace-DB";
 
-// setting cookies options
 export const OPTIONS = {
   httpOnly: true,
-  secure: true,
-  sameSite: "none",
-  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  secure: false,
+  sameSite: "lax",
+  path: "/",   // VERY IMPORTANT
 };
